0bd7d53d03c5359e03d53964e5c2f552ec3811df
prastoin <tkesray@gmail.com>
2022-01-30 22:22:46 +0100

    refactor: colorThemeSwitch using xstate and xstate/react

--- src/components/ColorThemeSwitch.tsx
+++ src/components/ColorThemeSwitch.tsx
@@ -1,38 +1,128 @@
-import { useEffect, useState } from 'react';
+import { useMachine } from '@xstate/react';
+import { assign, createMachine } from 'xstate';
 
 type ColorTheme = 'dark' | 'light';
 
-function getLocalStorageColorThemeValue(): ColorTheme {
-    if (typeof window === 'undefined') throw new Error('page not built');
-    return localStorage.getItem('color-theme') as ColorTheme;
-}
+const toggleColorThemeMachine = createMachine(
+    {
+        schema: {
+            context: {
+                colorTheme: undefined,
+            } as { colorTheme: undefined | ColorTheme },
+            events: {} as
+                | { type: '__RETRIEVED_COLOR_THEME'; colorTheme: ColorTheme }
+                | { type: 'TOGGLE' },
+        },
+        initial: 'retrievingColorThemeFromLocalStorage',
+        states: {
+            retrievingColorThemeFromLocalStorage: {
+                invoke: {
+                    src: 'retrieveColorThemeFromLocalStorage',
+                },
 
-function setLocalStorageColorThemeValue(value: ColorTheme) {
-    if (typeof window !== 'undefined') {
-        localStorage.setItem('color-theme', value);
-    }
-}
+                on: {
+                    __RETRIEVED_COLOR_THEME: [
+                        {
+                            cond: 'isDark',
+                            actions: 'assignDarkToColorTheme',
+                            target: 'dark',
+                        },
+                        {
+                            cond: 'isLight',
+                            actions: 'assignLightToColorTheme',
+                            target: 'light',
+                        },
+                    ],
+                },
+            },
+
+            dark: {
+                invoke: {
+                    src: 'addDarkFromDocumentClassList',
+                },
+
+                on: {
+                    TOGGLE: {
+                        target: 'light',
+                        actions: 'assignLightToColorTheme',
+                    },
+                },
+            },
+            light: {
+                invoke: {
+                    src: 'removeDarkFromDocumentClassList',
+                },
+
+                on: {
+                    TOGGLE: {
+                        target: 'dark',
+                        actions: 'assignDarkToColorTheme',
+                    },
+                },
+            },
+        },
+    },
+    {
+        guards: {
+            isDark: (_context, event) =>
+                event.type === '__RETRIEVED_COLOR_THEME' &&
+                event.colorTheme === 'dark',
+            isLight: (_context, event) =>
+                event.type === '__RETRIEVED_COLOR_THEME' &&
+                event.colorTheme === 'light',
+        },
+        services: {
+            removeDarkFromDocumentClassList: () => () => {
+                document.documentElement.classList.remove('dark');
+            },
+
+            addDarkFromDocumentClassList: () => () => {
+                document.documentElement.classList.add('dark');
+            },
+
+            retrieveColorThemeFromLocalStorage: () => (sendBack) => {
+                if (typeof window === 'undefined')
+                    throw new Error('page not built');
+                const localStorageColorThemeValue =
+                    localStorage.getItem('color-theme');
+
+                switch (localStorageColorThemeValue) {
+                    case 'light':
+                    case 'dark': {
+                        sendBack({
+                            type: '__RETRIEVED_COLOR_THEME',
+                            colorTheme: localStorageColorThemeValue,
+                        });
+                        break;
+                    }
+                    default: {
+                        sendBack({
+                            type: '__RETRIEVED_COLOR_THEME',
+                            colorTheme: 'light',
+                        });
+                    }
+                }
+            },
+        },
+        actions: {
+            assignDarkToColorTheme: assign({
+                colorTheme: () => 'dark' as ColorTheme,
+            }),
+            assignLightToColorTheme: assign({
+                colorTheme: () => 'light' as ColorTheme,
+            }),
+        },
+    },
+);
 
 const ColorThemeSwitch: React.FC = () => {
-    const [colorTheme, setColorTheme] = useState<ColorTheme>('dark');
+    const [state, send] = useMachine(toggleColorThemeMachine);
 
-    //TODO refactor using xstate
     const handleOnClick = () => {
-        if (colorTheme === 'dark') {
-            setColorTheme('light');
-            setLocalStorageColorThemeValue('light');
-            document.documentElement.classList.remove('dark');
-        } else {
-            setColorTheme('dark');
-            setLocalStorageColorThemeValue('dark');
-            document.documentElement.classList.add('dark');
-        }
+        send({
+            type: 'TOGGLE',
+        });
     };
-
-    useEffect(() => {
-        console.log('useEffect');
-        setColorTheme(getLocalStorageColorThemeValue());
-    }, []);
 
     return (
         <button
@@ -41,7 +131,7 @@ const ColorThemeSwitch: React.FC = () => {
                 handleOnClick();
             }}
         >
-            {colorTheme}
+            {state.context.colorTheme || 'loading'}
         </button>
     );
 };
