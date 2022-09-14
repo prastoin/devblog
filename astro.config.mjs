e56a5ea7aff8fada98ef8e64bb5ee58a1c3de316
prastoin <prastoin@student.42.fr>
2022-09-14 12:53:10 +0200

    refactor: deprecated renderers -> integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import astroRemark from '@astrojs/markdown-remark';
+import react from '@astrojs/react';
 
 export default {
   // Enable Custom Markdown options, plugins, etc.
-	markdownOptions: {
+	markdown: {
 		render: [
 			astroRemark,
 			{
@@ -19,17 +20,8 @@ export default {
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-      tailwindConfig: './tailwind.config.js',
-  },
-  renderers: [
-    '@astrojs/renderer-react'
+  // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  integrations: [
+    react(),
   ],
-  
 };
