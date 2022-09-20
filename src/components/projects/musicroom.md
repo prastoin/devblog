8c1fc12a251786a85ab04e1447e5d99b4490dac9
prastoin <prastoin@student.42.fr>
2022-09-20 13:13:30 +0200

    feat: add musicroom project refactor ProjectPost layout

---
+++ src/components/projects/musicroom.md
@@ -0,0 +1,12 @@
+---
+title: Musicroom
+imageName: musicroom
+publishDate: 18 may 2022
+githubRepoUrl: https://github.com/AdonisEnProvence/musicroom
+peopleCounter: 2
+productionUrl: https://musicroom.adonisenprovence.com/
+tags: [temporal, xstate, golang, expo]
+is42Project: true
+---
+
+This is the musicroom description
--- src/types/ProjectsType.ts
+++ src/types/ProjectsType.ts
@@ -1,5 +1,8 @@
 export interface ProjectFrontmatter {
     title: string
+    // Please not that the imageName must not includes the file extension
+    // For the moment we will only be using .jpg file
+    // See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
     imageName: string
     publishDate: string
     githubRepoUrl: string
